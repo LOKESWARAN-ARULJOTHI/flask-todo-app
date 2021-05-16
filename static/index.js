4f532677075ade0fcb88040015467cba679fd4d3
Lokeswaran Aruljothi <74011196+LOKESWARAN-ARULJOTHI@users.noreply.github.com>
2021-05-16 10:55:29 +0530

    commit templates

---
+++ static/index.js
@@ -0,0 +1,8 @@
+function deleteTodo(todoId) {
+    fetch("/delete-todo", {
+      method: "POST",
+      body: JSON.stringify({ todoId: todoId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
