6cbbb0176fb320d364e3986ba2e023c151e9ac60
Lokeswaran Aruljothi <74011196+LOKESWARAN-ARULJOTHI@users.noreply.github.com>
2021-05-16 11:23:30 +0530

    Delete website directory

--- website/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteTodo(todoId) {
-    fetch("/delete-todo", {
-      method: "POST",
-      body: JSON.stringify({ todoId: todoId }),
-    }).then((_res) => {
-      window.location.href = "/";
-    });
-  }
